f5505dc4db858566196c9c8f8c5b4af1c5b3442a
Tal Wrii <talwrii@gmail.com>
2025-05-13 06:49:56 +0200

    bump release

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ import distutils.core
 
 setuptools.setup(
     name='mapillary-nearby',
-    version="1.0.0",
+    version="1.1.0",
     author='@readwithai',
     long_description_content_type='text/markdown',
     author_email='talwrii@gmail.com',
-    description='Get pictures near an area',
+    description='Get pictures near an location using mapillary suitable for use as wallpaper.',
     license='MIT',
     keywords='mapilliary,wallpaper,background,nearby',
     url='https://github.com/talwrii/mapillary-nearby',
